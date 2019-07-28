cf9c39501363bd0952cc57cc0dcfcfeb36d733c2
Izzudin Anuar <izzudinanuar96@gmail.com>
2019-07-29 10:31:56 +1200

    Show job status on show page if auth user is owner

--- resources/views/jobs/show.blade.php
+++ resources/views/jobs/show.blade.php
@@ -4,6 +4,9 @@
     <div class="container">
         <h1>{{ $job->title }} <small>${{ $job->price / 100 }}</small></h1>
         <p>By {{ $job->author->name }} <small>{!! $job->isOwner() ? "- <a href=\"/jobs/" . $job->id . "/edit/\">Edit Job</a>" : '' !!}</small></p>
+        @if($job->isOwner())
+            <span class="badge badge-pill badge-info">{{ \App\Job::STATUS_TYPE[$job->status] }}</span>
+        @endif
         <div class="row">
             <img src="https://placehold.co/600x400" alt="">
         </div>
