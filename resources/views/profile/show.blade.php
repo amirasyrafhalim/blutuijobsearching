d6a6ce3a02aece798dde77cfbcba5e8f6f160571
Muneerah <muneerahazmi96@gmail.com>
2019-08-06 15:41:22 +1200

    Freelancer profile show data if neccessary.

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,19 +1,18 @@
 @extends('layouts.app')
 
 @section ('content')
-
     <div class="container">
         <h1>Freelancer Profile</h1>
         <div class="media border p-3">
-            <img src="https://via.placeholder.com/150" alt="Muneerah" class="img-thumbnail">
+            <img src="https://via.placeholder.com/150" alt="{{ $user->name }}" class="img-thumbnail">
             <div class="media-body">
                 <h4>{{ $user->name }}</h4>
                 <ul>
                     <li>
-                        From: {{$user->country}}
+                        From {{ $user->country }}
                     </li>
                     <li>
-                        Phone Number: {{$user -> phoneNum}}
+                        Phone Number {{ $user->phoneNum }}
                     </li>
                 </ul>
                 <div class="col text-right">
@@ -24,16 +23,26 @@
         <br>
         <div class="row">
             <div class="col-sm-8">
-                <h4>Description</h4>
-                <p>{{$user -> description}}</p>
-                <h4>Language</h4>
-                <p>{{$user->language}}</p>
-                <h4>Skills</h4>
-                <p>{{$user->skills}}</p>
-                <h4>Education</h4>
-                <p>{{$user->education}}</p>
-                <h4>Certificate</h4>
-                <p>{{$user->cert}}</p>
+                @if(!is_null($user->description))
+                    <h4>Description</h4>
+                    <p>{{ $user->description }}</p>
+                @endif
+                @if(!is_null($user->language))
+                    <h4>Language</h4>
+                    <p>{{ $user->language }}</p>
+                @endif
+                @if(!is_null($user->skills))
+                    <h4>Skills</h4>
+                    <p>{{ $user->skills }}</p>
+                @endif
+                @if(!is_null($user->education))
+                    <h4>Education</h4>
+                    <p>{{ $user->education }}</p>
+                @endif
+                @if(!is_null($user->cert))
+                    <h4>Certificate</h4>
+                    <p>{{ $user->cert }}</p>
+                @endif
             </div>
         </div>
     </div>
