8257e785d8f8363161f76878612a7059d0d3d35b
Izzudin Anuar <izzudinanuar96@gmail.com>
2019-09-05 22:09:36 +1200

    Fix codes

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -122,8 +122,6 @@ class JobsController extends Controller
     {
         $this->authorize('update', $job);
 
-
-
         $request->validate([
             'title' => 'required|min:5|max:255',
             'description' => 'required|min:5',
